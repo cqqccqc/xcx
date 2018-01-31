8c18e5c204d96227fd2d3b382cd0ec53ba8a7b83
q.chen <q.chen@ctrip.com>
2018-01-31 20:11:13 +0800

    market view

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-App({
+import pako from './lib/pako.min.js'
+const WS_URL = 'wss://api.huobi.pro/ws';
 
+App({
+  socketTask: null,
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
   onLaunch: function (options) {
-    
-    wx.showTabBar()
+    const self = this;
+    self.soketTask = wx.connectSocket({
+      url: WS_URL
+    });
+    self.soketTask.onOpen(() => {
+      console.log('WebSocket连接已打开！');
+
+      self.soketTask.send({
+        data: JSON.stringify({
+          "req": "market.btcusdt.kline.1min",
+          "id": "id10"
+        })
+      });
+      self.soketTask.onMessage((res) => {
+        let text = pako.inflate(res.data, {
+          to: 'string'
+        });
+        console.log(JSON.parse(text))
+        
+      })
+    });
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
   onShow: function (options) {
-    
+
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
   onHide: function () {
-    
+
   },
 
   /**
@@ -27,9 +49,5 @@ App({
    */
   onError: function (msg) {
     console.log(msg)
-  },
-
-  globalData: {
-
   }
 })
--- pages/fav/index.js
+++ pages/fav/index.js
@@ -1,25 +1,27 @@
-// pages/index/index.js
+// index.js
 Page({
+
+  webSocket: null,
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
